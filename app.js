96de20f325ea8d9337f795f803b625a1171d3c48
dhushordhrubo <dhushordhrubo@gmail.com>
2022-09-10 18:52:33 +0600

    Level 5 passport,cookies,session

--- app.js
+++ app.js
@@ -12,8 +12,13 @@ const mongoose = require('mongoose')
 // const md5 = require('md5');
 
 // Level 4 bcrypt Salting hashing
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+// const bcrypt = require('bcrypt');
+// const saltRounds = 10;
+
+// Level 5 passport.js
+const session = require('express-session');
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
 
 const app = express();
 
@@ -21,7 +26,17 @@ app.use(express.static('public'));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
-// MOngoose step -1 (MongoDB connection) 
+// Level 5 passport.js --- start
+app.use(session({
+    secret: 'this is my first projects',
+    resave: false,
+    saveUninitialized: false,
+  }))
+app.use(passport.initialize());
+app.use(passport.session());
+// Level 5 passport.js ---- end
+
+// Mongoose step -1 (MongoDB connection) 
 mongoURI =  "mongodb://localhost:27017/userDB" 
 mongoose.connect(mongoURI, { useNewUrlParser: true }) 
 .then(() => console.log("MongoDB connected")) 
@@ -33,6 +48,9 @@ const userSchema = new mongoose.Schema({
     password:String
 })
 
+// Level 5 passport.js 
+userSchema.plugin(passportLocalMongoose);
+
 // Level-2 (encryption)
 // const secret = process.env.SECRET
 // userSchema.plugin(encrypt, { secret: secret,encryptedFields: ['password'] });
@@ -40,6 +58,13 @@ const userSchema = new mongoose.Schema({
 
 //  Mongoose step -3 (Create Model)
 const User = new mongoose.model("User",userSchema);
+
+// Level 5 passport.js - start
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+// Level 5 passport.js - end
+
 
 app.get('/',(req,res) => {
     res.render ('home')});
@@ -52,55 +77,105 @@ app.get('/register',(req,res) => {
     res.render('register')
 })
 
-// User register
-app.post('/register',(req,res) => {
+app.get('/secrets',(req,res) => {
+    // Websecurity method to check authenticity.
+    if(req.isAuthenticated()){
+        res.render('secrets');
+    }else {
+        res.redirect('/login');
+    }
+})
 
-    // Level 4 bcrypt Salting hashing
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email:req.body.username,
-            // level 3 md5 hashing
-            // password:req.body.password
-            password: hash
-        })
-        // Check and render secrets
-        newUser.save(err => {
-            if(err){res.send(e)}
-        else{res.render('secrets')}  
-    })  
+app.get('/logout',(req,res)=> {
+    // Passport Method
+    req.logout(err => {
+        console.log(err)
     });
-});
+    res.redirect('/');
+})
 
-//User login
-app.post('/login',(req,res) => {
-    const username = req.body.username;
-    const password = req.body.password
-    // level 3 MD5 Hashing
-    // const password = md5(req.body.password);
-    
-    User.findOne({email:username},
-        (err,foundUser) => {
-            if(err){
-                console.log(err)
-                res.send(err)
-            }
-            else{
-                if(foundUser)
-              // Level 4 bcrypt Salting hashing
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                  if(result === true){
-                    res.render('secrets')
-                  }
-                });
+// // User register
+// app.post('/register',(req,res) => {
 
-                // Level 1  password comparision method
-                // { if(foundUser.password === password){
-                //     res.render('secrets')
-                // }}
-            }
+//     // Level 4 bcrypt Salting hashing
+//     bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+//         const newUser = new User({
+//             email:req.body.username,
+//             // level 3 md5 hashing
+//             // password:req.body.password
+//             password: hash
+//         })
+//         // Check and render secrets
+//         newUser.save(err => {
+//             if(err){res.send(e)}
+//         else{res.render('secrets')}  
+//     })  
+//     });
+// });
+
+// Level-5 passportjs - User register - start
+app.post('/register',(req,res) => {
+User.register({username:req.body.username}, req.body.password,(err,user) => {
+    if(err){
+        console.log(err);
+        res.redirect('/register')
+    }else{
+        passport.authenticate('local')(req,res,()=> {
+            res.redirect('/secrets');
         })
-}) 
+    }
+})
+})
+// Level-5 passportjs - User register - end
 
+// //User login
+// app.post('/login',(req,res) => {
+//     const username = req.body.username;
+//     const password = req.body.password
+//     // level 3 MD5 Hashing
+//     // const password = md5(req.body.password);
+    
+//     User.findOne({email:username},
+//         (err,foundUser) => {
+//             if(err){
+//                 console.log(err)
+//                 res.send(err)
+//             }
+//             else{
+//                 if(foundUser)
+//               // Level 4 bcrypt Salting hashing
+//                 bcrypt.compare(password, foundUser.password, function(err, result) {
+//                   if(result === true){
+//                     res.render('secrets')
+//                   }
+//                 });
+
+//                 // Level 1  password comparision method
+//                 // { if(foundUser.password === password){
+//                 //     res.render('secrets')
+//                 // }}
+//             }
+//         })
+// }) 
+
+// Level-5 passportjs - User login - start
+app.post('/login',(req,res) => {
+    const user = new User ({
+        username:req.body.username,
+        passsword:req.body.password
+    });
+    // Passport method
+    req.login(user,(err)=> {
+        if(err){
+            console.log(err)
+        }else{
+            passport.authenticate('local')(req,res,()=> {
+                res.redirect('/secrets');
+            })
+        }
+    })
+})
+// Level-5 passportjs - User login - End
 
 
 app.listen(3000,()=> {
